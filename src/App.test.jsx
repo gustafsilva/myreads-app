680affab8ed7f3103f0d62820e252a181fc70fa7
Gustavo Freitas <gustafsilva@hotmail.com>
2018-12-19 21:37:06 -0200

    Adicionando testes que modificam o estado da componente

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -13,6 +13,45 @@ describe('[component] App', () => {
     homePage: '/',
     searchPage: '/search',
   };
+  const LIST_BOOKS = [
+    {
+      title: 'The Linux Command Line',
+      authors: [
+        'William E. Shotts, Jr.',
+      ],
+      imageLinks: {
+        smallThumbnail: 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+        thumbnail: 'http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+      },
+      id: 'nggnmAEACAAJ',
+      shelf: 'read',
+    },
+    {
+      title: 'The Sebastian Thrun Handbook - Everything You Need to Know about Sebastian Thrun',
+      authors: [
+        'Andre Cantrell',
+      ],
+      imageLinks: {
+        smallThumbnail: 'http://books.google.com/books/content?id=NqkKvgAACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+        thumbnail: 'http://books.google.com/books/content?id=NqkKvgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+      },
+      id: 'NqkKvgAACAAJ',
+      shelf: 'wantToRead',
+    },
+    {
+      title: 'Travel by Design',
+      authors: [
+        'Marlon G. Boarnet',
+        'Randall Crane',
+      ],
+      imageLinks: {
+        smallThumbnail: 'http://books.google.com/books/content?id=f7EtoDfL6yYC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+        thumbnail: 'http://books.google.com/books/content?id=f7EtoDfL6yYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+      },
+      id: 'f7EtoDfL6yYC',
+      shelf: 'currentlyReading',
+    },
+  ];
 
   it('check init state', () => {
     const initStateExptected = {
@@ -78,6 +117,22 @@ describe('[component] App', () => {
 
       expect(myListsBooks).toHaveLength(lengthBooksExpected);
     });
+
+    it('check books rendering', () => {
+      const lengthBooksExpected = 3;
+
+      const wrapper = mount((
+        <MemoryRouter initialEntries={[PATHS.homePage]}>
+          <App />
+        </MemoryRouter>
+      ));
+      const appComponent = wrapper.find(App).instance();
+      appComponent.setState({ myBooks: LIST_BOOKS });
+      wrapper.update();
+      const booksSearch = wrapper.find(Book);
+
+      expect(booksSearch).toHaveLength(lengthBooksExpected);
+    });
   });
 
   describe('SearchPage', () => {
@@ -105,6 +160,22 @@ describe('[component] App', () => {
       const myListsBooks = wrapper.find(Book);
 
       expect(myListsBooks).toHaveLength(lengthBooksExpected);
+    });
+
+    it('check books rendering', () => {
+      const lengthBooksExpected = 3;
+
+      const wrapper = mount((
+        <MemoryRouter initialEntries={[PATHS.searchPage]}>
+          <App />
+        </MemoryRouter>
+      ));
+      const appComponent = wrapper.find(App).instance();
+      appComponent.setState({ booksSearch: LIST_BOOKS });
+      wrapper.update();
+      const booksSearch = wrapper.find(Book);
+
+      expect(booksSearch).toHaveLength(lengthBooksExpected);
     });
   });
 });
